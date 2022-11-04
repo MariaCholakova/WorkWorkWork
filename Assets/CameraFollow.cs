13b8c983f183dbfdfbfeff4825273041837fc4cd
Georgi Semkov <georgesemkov@gmail.com>
2022-11-04 23:24:55 +0200

    Add camera follow and character movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed;
+    public Vector3 offset;
+    public bool isFollow = true;
+
+    void LateUpdate()
+    {
+        if (isFollow)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+            //transform.LookAt(target);
+        }
+    }
+}
+
+
